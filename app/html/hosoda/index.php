026f775912f6a2b68696470a6cc15ea1519a9cf1
sayuusa <sayuusawithsasaraseikatu@yahoo.co.jp>
2019-05-22 22:06:07 +0900

    ひな形の作成

---
+++ app/html/hosoda/index.php
@@ -0,0 +1,3 @@
+<?php
+$html = file_get_contents("top.html");
+echo $html;
--- app/html/index.php
+++ app/html/index.php
@@ -1,3 +1,24 @@
 <?php
-$html = file_get_contents("html/index.html");
-echo $html;
+// ルーティング定義
+$url = $_SERVER['REQUEST_URI'];
+switch ($url) {
+    case '/':
+        $path = 'top.php';
+        break;
+    case '/eziri':
+        $path = 'eziri/index.php';
+        break;
+    case '/hosoda':
+        $path = '/hosoda/index.php';
+        break;
+    case 'kashimura':
+        $path = 'kashimura/index.php';
+        break;
+    case '/shimizu':
+        $path = 'shimizu/index.php';
+        break;
+    default:
+        echo 'お探しのページは見つかりませんでした。';
+}
+
+require($path);
